5ef86755618cb56fb0a7ef931bc28c1554b6fa83
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-09-11 11:26:07 +0530

    Initial commit, DropDown Button Not working

---
+++ lib/screens/note_detail.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+class NoteDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return NoteDetailState();
+  }
+}
+
+class NoteDetailState extends State<NoteDetail> {
+  static var _priorities = ['high', 'low'];
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Note'),
+      ),
+      body: Padding(
+          padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+          child: ListView(
+            children: <Widget>[
+              //First Element
+
+              ListTile(
+                title: DropdownButton(
+                  items: _priorities.map((String dropDownStringItem) {
+                    return DropdownMenuItem<String>(
+                      value: dropDownStringItem,
+                      child: Text(dropDownStringItem),
+                    );
+                  }).toList(),
+                  style: textStyle,
+                  value: 'Low',
+                  onChanged: (valueSelectedByUser) {
+                    setState(() {
+                      debugPrint('user selected $valueSelectedByUser');
+                    });
+                  },
+                ),
+              ),
+
+              //Second Element
+              Padding(
+                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                child: TextField(
+                  controller: titleController,
+                  style: textStyle,
+                  onChanged: (value) {
+                    debugPrint('You changed it to $value');
+                  },
+                  decoration: InputDecoration(
+                      labelText: 'Title',
+                      labelStyle: textStyle,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      )),
+                ),
+              ),
+
+              //Third Element
+              Padding(
+                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                child: TextField(
+                  controller: descriptionController,
+                  style: textStyle,
+                  onChanged: (value) {
+                    debugPrint('You changed it to $value');
+                  },
+                  decoration: InputDecoration(
+                      labelText: 'Description',
+                      labelStyle: textStyle,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      )),
+                ),
+              ),
+
+              //Fourth Element
+              Padding(
+                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                          'Save',
+                          textScaleFactor: 1.5,
+                        ),
+                        onPressed: () {
+                          debugPrint('Save button Clicked');
+                        },
+                      ),
+                    ),
+                    Container(
+                      width: 6.9,
+                    ),
+                    Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                          'Delete',
+                          textScaleFactor: 1.5,
+                        ),
+                        onPressed: () {
+                          debugPrint('Delete button Clicked');
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
